0f75ee16c50eb8880809a038b2c5668ef7179d7e
Michael Florez Rico  [Ubuntu] <michael.florez.r@gmail.com>
2024-08-14 01:22:59 -0500

    actualización de requerimientos pandas, y ajuste de proyecto app

--- app/main.py
+++ app/main.py
@@ -1,21 +1,32 @@
 import utils
 import read_csv
 import charts
+import pandas as pd
 
 def run():
-  data = read_csv.read_csv('world_population.csv')
   """
+  
+  
   data = list(filter(lambda item : item['Continent'] == 'South America',data))
 
   countries = list(map(lambda x: x['Country'], data))
   percentages = list(map(lambda x: x['World Population Percentage'], data))
-  charts.generate_pie_chart(countries, percentages)
+  
 """
+  df = pd.read_csv("world_population.csv")
+  df = df[df["Continent"]=="Africa"]
+
+  countries = df["Country"].values
+  percentages= df["World Population Percentage"].values
+  charts.generate_pie_chart(countries, percentages)
+
+  data = read_csv.read_csv('world_population.csv')
   country = input('Type Country => ').title()
   name = country
+  
 
   result = utils.population_by_country(data, country)
-
+  print(result)
   if len(result) > 0:
     country = result[0]
     labels, values = utils.get_population(country)
